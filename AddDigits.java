a3389f7a7d2eb99096fef9dfdff3b07e5f60001d
sandeshpatil75 <143398226+sandeshpatil75@users.noreply.github.com>
2025-03-10 17:36:20 +0530

    Create AddDigits.java

---
+++ AddDigits.java
@@ -0,0 +1,18 @@
+public class AddDigits {
+    public static int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;  
+                num /= 10;        
+            }
+            num = sum;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println("Final single digit: " + addDigits(num));
+    }
+}
